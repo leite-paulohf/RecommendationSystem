5d324523b377b3d9edd9649dbe947db730ed1e2a
Paulo Henrique Leite <paulo-http@live.com>
2019-10-17 04:31:56 +0100

    Implemented logic to show list of restaurants on screen

--- APP/lib/components/cell.dart
+++ APP/lib/components/cell.dart
@@ -12,8 +12,55 @@ class Cell extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: Colors.deepOrange,
+    return Padding(
+      padding: EdgeInsets.fromLTRB(8, 8, 8, 0),
+      child: Container(
+        color: Colors.teal,
+        child: Column(
+          children: <Widget>[
+            Text(
+              restaurant.name,
+              style: TextStyle(color: Colors.white),
+            ),
+            Text(
+              restaurant.discount.toString(),
+              style: TextStyle(color: Colors.white),
+            ),
+            Text(
+              restaurant.price.toString(),
+              style: TextStyle(color: Colors.white),
+            ),
+            Text(
+              restaurant.rating.toString(),
+              style: TextStyle(color: Colors.white),
+            ),
+            Text(
+              restaurant.kind.name,
+              style: TextStyle(color: Colors.white),
+            ),
+            Text(
+              restaurant.city.name,
+              style: TextStyle(color: Colors.white),
+            ),
+            Text(
+              restaurant.neighbourhood.name,
+              style: TextStyle(color: Colors.white),
+            ),
+            Text(
+              restaurant.cuisine.name,
+              style: TextStyle(color: Colors.white),
+            ),
+            Text(
+              restaurant.category.name,
+              style: TextStyle(color: Colors.white),
+            ),
+            Text(
+              restaurant.moment.name,
+              style: TextStyle(color: Colors.white),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
--- APP/lib/restaurants/restaurants.dart
+++ APP/lib/restaurants/restaurants.dart
@@ -1,4 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:scoped_model/scoped_model.dart';
+import 'package:tcc_app/components/table.dart';
+import 'package:tcc_app/helper/alert.dart';
+import 'package:tcc_app/helper/loader.dart';
+import 'package:tcc_app/model/restaurant.dart';
+import 'package:tcc_app/restaurants/viewmodel.dart';
+import 'package:tcc_app/service/restaurant.dart';
+import 'package:tcc_app/model/error.dart';
 
 class Restaurants extends StatefulWidget {
   Restaurants({Key key}) : super(key: key);
@@ -8,12 +16,55 @@ class Restaurants extends StatefulWidget {
 }
 
 class RestaurantsState extends State<Restaurants> {
+  final _key = GlobalKey<ScaffoldState>();
+  final viewModel = RestaurantViewModel(interface: RestaurantService());
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Container(color: Colors.black38),
-      ),
+      key: _key,
+      body: _body(),
     );
   }
+
+  @override
+  void dispose() {
+    super.dispose();
+  }
+
+  Widget _body() {
+    return FutureBuilder<List<Restaurant>>(
+      future: _search(),
+      builder: (context, snapshot) {
+        switch (snapshot.connectionState) {
+          case ConnectionState.done:
+            return ScopedModel<RestaurantViewModel>(
+                model: this.viewModel,
+                child: TableView(
+                  restaurants: snapshot.data ?? [],
+                  booking: _booking,
+                  favourite: _favourite,
+                ));
+          default:
+            return Loader().show();
+        }
+      },
+    );
+  }
+
+  Future<List<Restaurant>> _search() async {
+    var result = await this.viewModel.search(1);
+    var code = result.item1;
+    switch (code) {
+      case 200:
+        return result.item2;
+      default:
+        Alert.show(context, Error.from(code).message);
+        return [];
+    }
+  }
+
+  void _booking() {}
+
+  void _favourite() {}
 }
