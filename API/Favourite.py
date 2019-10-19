eed116033adb9f6ead83b115ab5e17555b1d3f83
Paulo Henrique Leite <paulo-http@live.com>
2019-10-19 17:54:23 +0100

    Implemented new endpoints

--- API/Favourite.py
+++ API/Favourite.py
@@ -1,38 +1,72 @@
+import uuid
 import sqlalchemy as sql
 from sqlalchemy import exc
 from flask import request, jsonify
 from flask_restful import Resource
-import json
+from Restaurants import Restaurants
 
 class Favourite(Resource):
 
     def __init__(self):
-        engine = sql.create_engine('sqlite:///database.db', echo = True)
+        engine = sql.create_engine('sqlite:///database.db', echo=True)
         metadata = sql.MetaData(engine)
         self.connection = engine.connect()
         self.database = sql.Table('favourite', metadata,
                                   sql.Column('uuid', sql.String, primary_key=True),
+                                  sql.Column('restaurant_uuid', sql.String),
                                   sql.Column('client_uuid', sql.String))
         metadata.create_all(engine)
 
-    def favourite(self):
-        favourite = request.json['favourite']
+    def add(self):
+        client_uuid = request.json['client_uuid']
+        restaurant_uuid = request.json['restaurant_uuid']
         query = sql.insert(self.database)
-        query = query.values(uuid=favourite['uuid'], client_uuid=favourite['client_uuid'])
+        query = query.values(uuid=str(uuid.uuid4()),
+                             restaurant_uuid=restaurant_uuid,
+                             client_uuid=client_uuid)
         try:
             self.connection.execute(query)
         except exc.SQLAlchemyError as error:
             return {'error': {'code': error.code, 'message': error.orig.args[0]}}
-        query = sql.select([self.database])
-        query = query.where(self.database.c.client_uuid == favourite['client_uuid'])
+        query = sql.select([self.database.c.restaurant_uuid])
+        query = query.where(self.database.c.client_uuid == client_uuid)
         data = self.connection.execute(query)
-        result = {'data': dict(zip(tuple(data.keys()), i)) for i in data.cursor}
+        result = {'data': [self.detail(list(i).pop()) for i in data.cursor]}
         return jsonify(result)
 
-    def favourites(self):
-        client_uuid = request.args.get('client_uuid')
-        query = sql.select([self.database.c.uuid])
+    def remove(self):
+        client_uuid = request.json['client_uuid']
+        restaurant_uuid = request.json['restaurant_uuid']
+        query = sql.delete(self.database)
+        query = query.where(self.database.c.restaurant_uuid == restaurant_uuid)
         query = query.where(self.database.c.client_uuid == client_uuid)
-        data = self.connection.execute(query).fetchall()
-        array = [dict(value) for value in data]
-        return json.dumps({'data': array})
+        try:
+            self.connection.execute(query)
+        except exc.SQLAlchemyError as error:
+            return {'error': {'code': error.code, 'message': error.orig.args[0]}}
+        query = sql.select([self.database.c.restaurant_uuid])
+        query = query.where(self.database.c.client_uuid == client_uuid)
+        data = self.connection.execute(query)
+        result = {'data': [self.detail(list(i).pop()) for i in data.cursor]}
+        return jsonify(result)
+
+    def getAll(self):
+        client_uuid = request.args.get('client_uuid')
+        query = sql.select([self.database.c.restaurant_uuid])
+        query = query.where(self.database.c.client_uuid == client_uuid)
+        data = self.connection.execute(query)
+        result = {'data': [self.detail(list(i).pop()) for i in data.cursor]}
+        return jsonify(result)
+
+    def detail(self, uuid):
+        restaurants = Restaurants()
+        query = sql.select([restaurants.database])
+        query = query.where(restaurants.database.c.uuid == uuid)
+        data = restaurants.connection.execute(query)
+        result = {'data': dict(zip(tuple(data.keys()), i)) for i in data.cursor}
+        if bool(result):
+            restaurant = result['data']
+            restaurants.parse(restaurant)
+            return restaurant
+        else:
+            return {}
