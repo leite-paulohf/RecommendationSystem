81b33985d475de4e1d00a8cc14974957361af2ae
Paulo Henrique Leite <paulo-http@live.com>
2020-01-28 13:27:53 +0000

    Updated localhost path

--- APP/lib/service/service.dart
+++ APP/lib/service/service.dart
@@ -17,7 +17,7 @@ class Service {
 
   Service._internal();
 
-  final _base = 'f412d948.ngrok.io';
+  final _base = '0c29950f.ngrok.io';
   final _lock = Lock();
 
   Future<http.Response> get(String path, Map<String, String> data) async {
